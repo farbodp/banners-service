f772702a2209946343d1ca176847b17e73f9c1fc
Farbod Parvin <farbod.parvin72@gmail.com>
2023-06-26 17:05:02 +0330

    refactor read_csv_remove_duplicates

--- utils/utils.py
+++ utils/utils.py
@@ -4,32 +4,35 @@ from functools import lru_cache
 import datetime
 
 
-# @lru_cache(maxsize=32)
 def read_csv_remove_duplicates(file_path, log_file_path=None):
-    # Read CSV file into a pandas DataFrame
     df = pd.read_csv(file_path)
 
-    # Identify duplicate rows in the DataFrame
-    duplicates = df[df.duplicated()]
+    if log_file_path:
+        log_duplicates(file_path, df, log_file_path)
 
-    # Log duplicate rows to a file using the Python logging module if log_file_path is provided
-    if log_file_path is not None:
-        logger = logging.getLogger(__name__)
-        logger.setLevel(logging.DEBUG)
-        file_handler = logging.FileHandler(log_file_path)
-        file_handler.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
-
-        if not duplicates.empty:
-            logger.debug(f"Found {len(duplicates)} duplicate rows in {file_path}:\n{duplicates.to_string()}")
-
-    # Remove duplicate rows from the DataFrame
-    df.drop_duplicates(inplace=True)
+    remove_duplicates(df)
 
     return df
 
+def log_duplicates(file_path, df, log_file_path):
+    duplicates = df[df.duplicated()]
+    if duplicates.empty:
+        return
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler(log_file_path)
+    file_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.debug(f"Found {len(duplicates)} duplicate rows in {file_path}:\n{duplicates.to_string()}")
+
+def remove_duplicates(df):
+    df.drop_duplicates(inplace=True)
 
 def get_hour_quarter():
     now = datetime.datetime.utcnow()
